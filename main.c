305fcf1a08d6bd9e2b97adccb1cb26bf82739869
Michael Buesch <mb@bu3sch.de>
2008-08-29 19:43:30 +0200

    Ass support for multiple inputs driving one output.
    Signed-off-by: Michael Buesch <mb@bu3sch.de>

--- main.c
+++ main.c
@@ -1,12 +1,6 @@
 /*
  * Signal debouncer
  *
- * The debouncing strategy is:
- * 	Trigger OUT immediately when IN is logical 1.
- * 	Ignore the IN signal for a certain dwell time.
- * 	Switch OUT off after the dwell time, if IN is no longer logical 1.
- * 	This ensures a low logical 1 latency.
- *
  * Copyright (c) 2008 Michael Buesch <mb@bu3sch.de>
  *
  * Licensed under the GNU General Public License version 2 or later.
@@ -19,103 +13,155 @@
 
 
 #define ARRAY_SIZE(x)		(sizeof(x) / sizeof((x)[0]))
+
+/* Memory barrier.
+* The CPU doesn't have runtime reordering, so we just
+* need a compiler memory clobber. */
+#define mb()			__asm__ __volatile__("" : : : "memory")
+
 typedef _Bool			bool;
 
 
+#define DEBOUNCE_DWELL_TIME	10 /* centiseconds */
+
+#if DEBUG
+# undef DEBOUNCE_DWELL_TIME
+# define DEBOUNCE_DWELL_TIME	200
+#endif
+
+
 /**
- * struct pin - Digital signal line for debouncing
+ * struct input_pin - An input pin definition
  *
  * @input_port:		The signal input port. PORTB, PORTC, ...
  * @input_pin:		The signal input pin. PINB, PINC, ...
  * @input_ddr:		Data direction register for input_port.
  * @input_bit:		The bit number on the input_port.
- *
- * @output_port:	The signal output port. PORTB, PORTC, ...
- * @output_ddr:		Data direction register for output_port.
- * @output_bit:		The bit number on the output_port.
- *
- * @flags:		INPUT_PULLUP, OUTPUT_INVERT.
+ * @flags:		See enum input_pin_flags.
  */
-struct pin {
+struct input_pin {
 	uint8_t input_port;
 	uint8_t input_pin;
 	uint8_t input_ddr;
 	uint8_t input_bit;
+	uint8_t flags;
+};
+/**
+ * enum input_pin_flags - Flags for an input pin
+ *
+ * @INPUT_PULLUP:	Use pullups for the input pin.
+ * @INPUT_INVERT:	Logically invert the input signal.
+ */
+enum input_pin_flags {
+	INPUT_PULLUP		= (1 << 0),
+	INPUT_INVERT		= (1 << 1),
+};
+
+/**
+ * struct output_pin - Level triggered output pin
+ *
+ * @output_port:	The signal output port. PORTB, PORTC, ...
+ * @output_ddr:		Data direction register for output_port.
+ * @output_bit:		The bit number on the output_port.
+ * @flags:		See enum output_pin_flags.
+ */
+struct output_pin {
 	uint8_t output_port;
 	uint8_t output_ddr;
 	uint8_t output_bit;
 	uint8_t flags;
 
+	/* Trigger level */
+	uint8_t level;
+};
+/**
+ * enum output_pin_flags - Flags for an output pin
+ *
+ * @OUTPUT_INVERT:	Logically invert the output signal.
+ */
+enum output_pin_flags {
+	OUTPUT_INVERT		= (1 << 0),
+};
+
+/**
+ * struct connection - Logical connection between input and output pins
+ *
+ * @in:		Definition of the input pin.
+ * @out:	Pointer to the output pin.
+ */
+struct connection {
+	struct input_pin in;
+	struct output_pin *out;
+
+	bool input_is_asserted;
+	bool output_is_asserted;
 	uint8_t dwell_time;
 };
 
-enum { /* flags */
-	/* Static configuration flags */
-	INPUT_PULLUP		= (1 << 0),
-	OUTPUT_INVERT		= (1 << 1),
+#define DEF_INPUT(portid, bit, _flags)				\
+	.in = {							\
+		.input_port	= _SFR_ADDR(PORT##portid),	\
+		.input_pin	= _SFR_ADDR(PIN##portid),	\
+		.input_ddr	= _SFR_ADDR(DDR##portid),	\
+		.input_bit	= bit,				\
+		.flags		= _flags			\
+	}
 
-	/* Dynamic runtime flags.
-	 * Do not set in static config below. */
-	IS_ASSERTED		= (1 << 2),
-	LOGICAL_OUT_ON		= (1 << 3),
-};
+#define DEF_OUTPUT(portid, bit, _flags)				\
+	struct output_pin output_pin_##portid##bit = {		\
+		.output_port	= _SFR_ADDR(PORT##portid),	\
+		.output_ddr	= _SFR_ADDR(DDR##portid),	\
+		.output_bit	= bit,				\
+		.flags		= _flags,			\
+	}
+#define NONE	0
 
-#define DEF_INPUT(portid, bit)				\
-	.input_port	= _SFR_ADDR(PORT##portid),	\
-	.input_pin	= _SFR_ADDR(PIN##portid),	\
-	.input_ddr	= _SFR_ADDR(DDR##portid),	\
-	.input_bit	= bit
+static DEF_OUTPUT(B, 0, NONE);
+static DEF_OUTPUT(D, 7, NONE);
+static DEF_OUTPUT(D, 6, NONE);
+static DEF_OUTPUT(D, 5, NONE);
+static DEF_OUTPUT(D, 4, NONE);
+static DEF_OUTPUT(D, 3, NONE);
+static DEF_OUTPUT(D, 2, NONE);
+static DEF_OUTPUT(D, 1, NONE);
+static DEF_OUTPUT(D, 0, NONE);
 
-#define DEF_OUTPUT(portid, bit)				\
-	.output_port	= _SFR_ADDR(PORT##portid),	\
-	.output_ddr	= _SFR_ADDR(DDR##portid),	\
-	.output_bit	= bit
-
-static struct pin pins[] = {
+static struct connection connections[] = {
 	{ /* B1 -> B0 */
-		DEF_INPUT(B, 1),
-		DEF_OUTPUT(B, 0),
-		.flags		= INPUT_PULLUP,
+		DEF_INPUT(B, 1, INPUT_PULLUP),
+		.out = &output_pin_B0,
 	},
 	{ /* B2 -> D7 */
-		DEF_INPUT(B, 2),
-		DEF_OUTPUT(D, 7),
-		.flags		= INPUT_PULLUP,
+		DEF_INPUT(B, 2, INPUT_PULLUP),
+		.out = &output_pin_D7,
 	},
 	{ /* B3 -> D6 */
-		DEF_INPUT(B, 3),
-		DEF_OUTPUT(D, 6),
-		.flags		= INPUT_PULLUP,
+		DEF_INPUT(B, 3, INPUT_PULLUP),
+		.out = &output_pin_D6,
 	},
 	{ /* B4 -> D5 */
-		DEF_INPUT(B, 4),
-		DEF_OUTPUT(D, 5),
-		.flags		= INPUT_PULLUP,
+		DEF_INPUT(B, 4, INPUT_PULLUP),
+		.out = &output_pin_D5,
 	},
 	{ /* C0 -> D4 */
-		DEF_INPUT(C, 0),
-		DEF_OUTPUT(D, 4),
-		.flags		= INPUT_PULLUP,
+		DEF_INPUT(C, 0, INPUT_PULLUP),
+		.out = &output_pin_D4,
 	},
 	{ /* C1 -> D3 */
-		DEF_INPUT(C, 1),
-		DEF_OUTPUT(D, 3),
-		.flags		= INPUT_PULLUP,
+		DEF_INPUT(C, 1, INPUT_PULLUP),
+		.out = &output_pin_D3,
 	},
 	{ /* C2 -> D2 */
-		DEF_INPUT(C, 2),
-		DEF_OUTPUT(D, 2),
-		.flags		= INPUT_PULLUP,
+		DEF_INPUT(C, 2, INPUT_PULLUP),
+		.out = &output_pin_D2,
 	},
 	{ /* C3 -> D1 */
-		DEF_INPUT(C, 3),
-		DEF_OUTPUT(D, 1),
-		.flags		= INPUT_PULLUP,
+		DEF_INPUT(C, 3, INPUT_PULLUP),
+		.out = &output_pin_D1,
 	},
 	{ /* C4 -> D0 */
-		DEF_INPUT(C, 4),
-		DEF_OUTPUT(D, 0),
-		.flags		= INPUT_PULLUP,
+		DEF_INPUT(C, 4, INPUT_PULLUP),
+		.out = &output_pin_D0,
 	},
 };
 
@@ -127,84 +173,106 @@ static struct pin pins[] = {
 
 #define MMIO8(mem_addr)		_MMIO_BYTE((uint16_t)(mem_addr))
 
-/* Set the output pin to a new state */
-static inline void output_set(struct pin *pin, bool state)
-{
-	if (state)
-		pin->flags |= LOGICAL_OUT_ON;
-	else
-		pin->flags &= ~LOGICAL_OUT_ON;
 
-	if (pin->flags & OUTPUT_INVERT)
+/* Set the hardware state of an output pin. */
+static inline void output_hw_set(struct output_pin *out, bool state)
+{
+	if (out->flags & OUTPUT_INVERT)
 		state = !state;
 	if (state)
-		MMIO8(pin->output_port) |= (1 << pin->output_bit);
+		MMIO8(out->output_port) |= (1 << out->output_bit);
 	else
-		MMIO8(pin->output_port) &= ~(1 << pin->output_bit);
+		MMIO8(out->output_port) &= ~(1 << out->output_bit);
+}
+
+/* Increment the trigger level of an output. */
+static inline void output_level_inc(struct output_pin *out)
+{
+	if (out->level == 0)
+		output_hw_set(out, 1);
+	out->level++;
+}
+
+/* Decrement the trigger level of an output. */
+static inline void output_level_dec(struct output_pin *out)
+{
+	out->level--;
+	if (out->level == 0)
+		output_hw_set(out, 0);
 }
 
 static void setup_ports(void)
 {
-	struct pin *pin;
+	struct connection *conn;
 	uint8_t i;
 
-	for (i = 0; i < ARRAY_SIZE(pins); i++) {
-		pin = &(pins[i]);
+	for (i = 0; i < ARRAY_SIZE(connections); i++) {
+		conn = &(connections[i]);
 
 		/* Init DDR registers */
-		MMIO8(pin->input_ddr) &= ~(1 << pin->input_bit);
-		MMIO8(pin->output_ddr) |= (1 << pin->output_bit);
+		MMIO8(conn->in.input_ddr) &= ~(1 << conn->in.input_bit);
+		MMIO8(conn->out->output_ddr) |= (1 << conn->out->output_bit);
+		conn->input_is_asserted = 0;
 
 		/* Enable/Disable pullup */
-		if (pin->flags & INPUT_PULLUP)
-			MMIO8(pin->input_port) |= (1 << pin->input_bit);
+		if (conn->in.flags & INPUT_PULLUP)
+			MMIO8(conn->in.input_port) |= (1 << conn->in.input_bit);
 		else
-			MMIO8(pin->input_port) &= ~(1 << pin->input_bit);
+			MMIO8(conn->in.input_port) &= ~(1 << conn->in.input_bit);
 
 		/* Disable output signal */
-		output_set(pin, 0);
+		conn->out->level = 0;
+		conn->output_is_asserted = 0;
+		output_hw_set(conn->out, 0);
 	}
 }
 
-static void scan_one_pin(struct pin *pin)
+static void scan_one_input_pin(struct connection *conn)
 {
 	uint8_t hw_input_asserted;
 
 	/* Get the input state */
-	hw_input_asserted = (MMIO8(pin->input_pin) & (1 << pin->input_bit));
-	if (pin->flags & INPUT_PULLUP) {
+	hw_input_asserted = (MMIO8(conn->in.input_pin) & (1 << conn->in.input_bit));
+	if (conn->in.flags & INPUT_PULLUP) {
 		/* With pullup the logical state is flipped */
 		hw_input_asserted = !hw_input_asserted;
 	}
+	if (conn->in.flags & INPUT_INVERT) {
+		/* User requested invert */
+		hw_input_asserted = !hw_input_asserted;
+	}
 
-	if (pin->flags & IS_ASSERTED) {
+	if (conn->input_is_asserted) {
 		/* Signal currently is asserted in software.
 		 * Wait for the dwell time... */
 		cli();
 		if (hw_input_asserted) {
 			/* The hardware pin is still active,
 			 * restart the dwell time. */
-			pin->dwell_time = 0;
+			conn->dwell_time = 0;
 		}
-		if (pin->dwell_time < 10) {
+		if (conn->dwell_time < DEBOUNCE_DWELL_TIME) {
 			sei();
 			/* wait... */
 			return;
 		}
-		pin->flags &= ~IS_ASSERTED;
+		conn->input_is_asserted = 0;
 		sei();
 	}
 
 	if (hw_input_asserted) {
-		if (!(pin->flags & LOGICAL_OUT_ON))
-			output_set(pin, 1);
-		cli();
-		pin->flags |= IS_ASSERTED;
-		pin->dwell_time = 0;
-		sei();
+		if (!conn->output_is_asserted) {
+			conn->output_is_asserted = 1;
+			output_level_inc(conn->out);
+		}
+		conn->dwell_time = 0;
+		mb();
+		conn->input_is_asserted = 1;
 	} else {
-		if (pin->flags & LOGICAL_OUT_ON)
-			output_set(pin, 0);
+		if (conn->output_is_asserted) {
+			conn->output_is_asserted = 0;
+			output_level_dec(conn->out);
+		}
 	}
 }
 
@@ -213,8 +281,8 @@ static void scan_input_pins(void)
 	uint8_t i;
 
 	while (1) {
-		for (i = 0; i < ARRAY_SIZE(pins); i++) {
-			scan_one_pin(&(pins[i]));
+		for (i = 0; i < ARRAY_SIZE(connections); i++) {
+			scan_one_input_pin(&(connections[i]));
 			wdt_reset();
 		}
 	}
@@ -223,14 +291,14 @@ static void scan_input_pins(void)
 /* System jiffies timer at 100Hz */
 ISR(TIMER1_COMPA_vect)
 {
-	struct pin *pin;
+	struct connection *conn;
 	uint8_t i;
 
-	for (i = 0; i < ARRAY_SIZE(pins); i++) {
-		pin = &(pins[i]);
-		if (pin->flags & IS_ASSERTED) {
-			if (pin->dwell_time < 0xFF)
-				pin->dwell_time++;
+	for (i = 0; i < ARRAY_SIZE(connections); i++) {
+		conn = &(connections[i]);
+		if (conn->input_is_asserted) {
+			if (conn->dwell_time < 0xFF)
+				conn->dwell_time++;
 		}
 	}
 
@@ -257,8 +325,9 @@ static void setup_jiffies(void)
 int main(void)
 {
 	cli();
-
+#if !DEBUG
 	wdt_enable(WDTO_500MS);
+#endif
 	wdt_reset();
 
 	setup_jiffies();
